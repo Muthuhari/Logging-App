c616b5a793e2479f2b8f5cc1298af53d21da56a4
MUthuhari <muthuharinisansala2@gmail.com>
2022-12-02 17:25:01 +0530

    new commit 1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 const Login = () => {
+
+
+    
     return (
         <LoginContainer>
             <Backimg><img src="./img/bg.jpg" alt="background" />
---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7jEySPA6d2-Qs-XnDiDKJRnkVlM4DQI0",
+  authDomain: "merncrud-app.firebaseapp.com",
+  projectId: "merncrud-app",
+  storageBucket: "merncrud-app.appspot.com",
+  messagingSenderId: "11993080338",
+  appId: "1:11993080338:web:b3821bf617d995e6d9eecb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
